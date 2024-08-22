773abde3344cee303d66af9a1055a3a33fdc7967
Hayden <haydengalyeanbiz@gmail.com>
2024-08-22 01:50:01 -0400

    fixed migration file, added os

--- migrations/versions/1f3596deb787_init_migration_1.py
+++ migrations/versions/1f3596deb787_init_migration_1.py
@@ -8,6 +8,9 @@ Create Date: 2024-08-22 01:39:18.362061
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '1f3596deb787'
