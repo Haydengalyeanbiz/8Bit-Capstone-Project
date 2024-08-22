dc732ac22e153fd0c46996c7b291adc737ec714b
Hayden <haydengalyeanbiz@gmail.com>
2024-08-22 02:18:59 -0400

    changed a few models

--- app/models/listing_category.py
+++ app/models/listing_category.py
@@ -1,10 +1,11 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class ListingCategory(db.Model):
-    __tablename__ = add_prefix_for_prod('listing_categories')
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+    __tablename__ = 'listing_categories'
 
     id = db.Column(db.Integer, primary_key=True)
     listing_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('listings.id')))
     category_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('categories.id')))
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
