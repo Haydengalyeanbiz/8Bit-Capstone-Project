8ecfe71786f706936ed56637e09f258f216663ba
Hayden <haydengalyeanbiz@gmail.com>
2024-08-22 02:21:48 -0400

    changed order of models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,9 @@
 from .db import db, environment, SCHEMA
 from .user import User
-from .cart_items import CartItem
-from .cart import Cart
 from .listing import Listing
+from .category import Category  
+from .listing_category import ListingCategory
+from .cart import Cart
+from .cart_items import CartItem
 from .wishlist import Wishlist
 from .review import Review
-from .listing_category import ListingCategory
-from .category import Category  
